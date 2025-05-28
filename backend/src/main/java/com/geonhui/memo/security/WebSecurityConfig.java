8533c2fbe8f25613bd936b308bc4ef74b579c452
Spooder <yoshi7591@naver.com>
2025-05-28 13:10:14 +0900

    feat: 백엔드 프로젝트를 구축하고, Docker를 설정했어요

---
+++ backend/src/main/java/com/geonhui/memo/security/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.geonhui.memo.security;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/**").permitAll()
+				.anyRequest().authenticated()
+			)
+            .csrf((csrf) -> csrf.disable());
+
+		return http.build();
+	}
+}
